61432a2fdd1e6917fbf27e6f01fd76e9c0ce9969
Jordan Pectyo <mrpectyo@gmail.com>
2024-01-04 14:46:53 -0500

    Closing websocket after completed messaging

--- services/sd-api.js
+++ services/sd-api.js
@@ -1,12 +1,9 @@
 const axios = require('axios');
-const { sdKey  } = require('../config.json');
 const { workflow } = require('../common/workflows/default.json');
 const { Attachment, AttachmentBuilder } = require('discord.js');
 const querystring = require('node:querystring');
 const crypto = require("node:crypto");
 const WebSocket = require('ws');
-
-const baseUri = "https://stablediffusionapi.com/api/v3";
 const serverAddr = "127.0.0.1:8188";
 const client_id = crypto.randomUUID().toString();
 
@@ -16,97 +13,108 @@ exports.queuePrompt = async(prompt) => {
         url: `http://${serverAddr}/prompt`,
         data: {
             "prompt": {
-                "57": {
-                  "inputs": {
-                    "ckpt_name": "perfectdeliberate_v40.safetensors"
-                  },
-                  "class_type": "CheckpointLoaderSimple"
+              "57": {
+                "inputs": {
+                  "ckpt_name": "dreamshaperXL_turboDpmppSDE.safetensors"
                 },
-                "58": {
-                  "inputs": {
-                    "seed": 829086049819405,
-                    "steps": 30,
-                    "cfg": 8,
-                    "sampler_name": "euler",
-                    "scheduler": "normal",
-                    "denoise": 1,
-                    "model": [
-                      "57",
-                      0
-                    ],
-                    "positive": [
-                      "59",
-                      0
-                    ],
-                    "negative": [
-                      "60",
-                      0
-                    ],
-                    "latent_image": [
-                      "61",
-                      0
-                    ]
-                  },
-                  "class_type": "KSampler"
+                "class_type": "CheckpointLoaderSimple"
+              },
+              "58": {
+                "inputs": {
+                  "seed": Math.random() * 19999,
+                  "steps": 10,
+                  "cfg": 3,
+                  "sampler_name": "euler",
+                  "scheduler": "normal",
+                  "denoise": 1,
+                  "model": [
+                    "57",
+                    0
+                  ],
+                  "positive": [
+                    "84",
+                    0
+                  ],
+                  "negative": [
+                    "60",
+                    0
+                  ],
+                  "latent_image": [
+                    "61",
+                    0
+                  ]
                 },
-                "59": {
-                  "inputs": {
-                    "text": "masterpiece best quality, girl, lovely evening dress, long black hair, solid white background",
-                    "clip": [
-                      "57",
-                      1
-                    ]
-                  },
-                  "class_type": "CLIPTextEncode"
+                "class_type": "KSampler"
+              },
+              "60": {
+                "inputs": {
+                  "text": "extra limbs, watermark, ugly",
+                  "clip": [
+                    "57",
+                    1
+                  ]
                 },
-                "60": {
-                  "inputs": {
-                    "text": "extra limbs, watermark, ugly",
-                    "clip": [
-                      "57",
-                      1
-                    ]
-                  },
-                  "class_type": "CLIPTextEncode"
+                "class_type": "CLIPTextEncode"
+              },
+              "61": {
+                "inputs": {
+                  "width": 1024,
+                  "height": 1024,
+                  "batch_size": 1
                 },
-                "61": {
-                  "inputs": {
-                    "width": 512,
-                    "height": 512,
-                    "batch_size": 1
-                  },
-                  "class_type": "EmptyLatentImage"
+                "class_type": "EmptyLatentImage"
+              },
+              "62": {
+                "inputs": {
+                  "samples": [
+                    "58",
+                    0
+                  ],
+                  "vae": [
+                    "57",
+                    2
+                  ]
                 },
-                "62": {
-                  "inputs": {
-                    "samples": [
-                      "58",
-                      0
-                    ],
-                    "vae": [
-                      "79",
-                      0
-                    ]
-                  },
-                  "class_type": "VAEDecode"
+                "class_type": "VAEDecode"
+              },
+              "77": {
+                "inputs": {
+                  "filename_prefix": "chat-bot",
+                  "images": [
+                    "62",
+                    0
+                  ]
                 },
-                "77": {
-                  "inputs": {
-                    "filename_prefix": "chat-bot",
-                    "images": [
-                      "62",
-                      0
-                    ]
-                  },
-                  "class_type": "SaveImage"
+                "class_type": "SaveImage"
+              },
+              "79": {
+                "inputs": {
+                  "vae_name": "vae-ft-mse-840000-ema-pruned.safetensors"
                 },
-                "79": {
-                  "inputs": {
-                    "vae_name": "vae-ft-mse-840000-ema-pruned.safetensors"
-                  },
-                  "class_type": "VAELoader"
-                }
+                "class_type": "VAELoader"
+              },
+              "82": {
+                "inputs": {
+                  "text": "a __demeanor__ __race__ wearing clothing that is __outfits__",
+                  "seed": Math.random() * 19999,
+                  "autorefresh": "Yes"
+                },
+                "class_type": "DPRandomGenerator"
+              },
+              "84": {
+                "inputs": {
+                  "text": [
+                    "82",
+                    0
+                  ],
+                  "clip": [
+                    "57",
+                    1
+                  ]
+                },
+                "class_type": "CLIPTextEncode"
               }
+            }
         }        
       })
 }
@@ -119,7 +127,6 @@ exports.getHistory = async(prompt_id) => {
 }
 
 exports.getImage = async(filename, subfolder, folder_type) => {
-    console.log(`http://${serverAddr}/view?${querystring.stringify({filename, subfolder, folder_type})}`)
     const resp =  axios.get(`http://${serverAddr}/view?${querystring.stringify({filename, subfolder, folder_type})}`,
     {responseType: 'arraybuffer'}
     );
@@ -129,15 +136,14 @@ exports.getImage = async(filename, subfolder, folder_type) => {
 exports.gatherImages = async(ws, prompt, interaction) => {
     console.log('Beginning image creation step')
     const {data: {prompt_id}} = await this.queuePrompt(prompt);    
-    let inProgress = false;
-    let runStarted = false;            
+    let queuedPrompt = true;
+    let skipQueueCheck = false;
     const onMessage = async (data) => {
         const parsed = JSON.parse(data);
         let output_images = [];
-        if(parsed?.data?.status?.exec_info?.queue_remaining === 1) {
-            inProgress = true;
-            runStarted = true;
-        } else if(runStarted && inProgress && parsed?.data?.status?.exec_info?.queue_remaining === 0) {
+        if(queuedPrompt && parsed?.data?.status?.exec_info?.queue_remaining === 1) {
+          skipQueueCheck = true;
+        } else if(skipQueueCheck && parsed?.data?.status?.exec_info?.queue_remaining === 0) {
             console.log('Image has been created processing now')
             let {data: history} = await this.getHistory(prompt_id);
             history = history[prompt_id]
@@ -146,16 +152,14 @@ exports.gatherImages = async(ws, prompt, interaction) => {
                     let node_output = history['outputs'][nodeId];
                     if (node_output.images.length > 0) {
                         for (const image of node_output['images']){
-                            console.dir(image)
-                            const image_data = await this.getImage(image['filename'], image['subfolder'], image['type'])
-                            console.log('Type of data', typeof image_data.data)
-                            const imageAttachment = new AttachmentBuilder(image_data.data)
-                            output_images.push(imageAttachment)
+                          const image_data = await this.getImage(image['filename'], image['subfolder'], image['type'])
+                          const imageAttachment = new AttachmentBuilder(image_data.data)
+                          output_images.push(imageAttachment)
                         }
                     }
                     
                 }
-            }
+            }            
             await interaction.followUp({content: 'Here you go!', files: output_images})
             ws.removeListener('message', onMessage)
             ws.close();
